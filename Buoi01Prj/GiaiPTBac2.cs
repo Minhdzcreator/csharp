dc9f208ce3c3341abb522d08f80dbcce81f9fc71
Minhdzcreator <vuvanminh54321@gmail.com>
2026-09-14 10:40:02 +0700

    Hoan thanh bai thuc hanh

---
+++ Buoi01Prj/GiaiPTBac2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MyLib;
+
+namespace Buoi01Prj;
+
+public class GiaiPTBac2
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Nhap a: ");
+        
+        double a = double.Parse(Console.ReadLine()!); 
+
+        Console.Write("Nhap b: ");
+        double b = double.Parse(Console.ReadLine()!);
+
+        Console.Write("Nhap c: ");
+        double c = double.Parse(Console.ReadLine()!);
+
+        double x1 = 0, x2 = 0;
+        int sn = LibBaiTap.GiaiPTBac2(a, b, c, ref x1, ref x2);
+
+        Console.WriteLine($"So nghiem: {sn}");
+        Console.WriteLine($"x1 = {x1}");
+        Console.WriteLine($"x2 = {x2}");
+    }
+}
---
+++ Buoi01Prj/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Buoi01Prj;
+class Program
+{
+static void Main(string[] args)
+{
+    Bai1.Run();
+    // Bai2.Run();
+}
+}
+class Bai1
+{
+   public static void Run()
+{
+   Console.WriteLine("Chay bai 1");
+}
+}
